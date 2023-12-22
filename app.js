2dbcd3c37947cf0114f210c876629146e6ae4fae
DeviousPear <146679233+DeviousPear@users.noreply.github.com>
2023-12-22 10:19:32 -0800

    Update app.js'

--- app.js
+++ app.js
@@ -16,18 +16,7 @@ var proxy = require("http").createServer((req, res) => {
         res.setHeader("Content-Type", "application/json")
         res.end(JSON.stringify(req.headers))
     } else if (req.headers.cookie && req.headers.cookie.match(/pearproxy=http/)) {
-        let fakeHeaders = {}
-        fakeHeaders["User-Agent"] = req.headers["user-agent"]
-        console.log("proxy on")
-        console.log(req.url)
-        let url = new URL(require("cookie").parse(req.headers.cookie).pearproxy)
-        if (url.hostname == "games.poki.com") {
-            fakeHeaders = req.headers
-            delete fakeHeaders["X-Real-IP"]
-            delete fakeHeaders["X-Forwarded-For"]
-            console.log("poki games url")
-        }
-        require(url.protocol.split(":")[0]).get(url.origin + req.url, {headers: fakeHeaders}, (resp => {
+        require(url.protocol.split(":")[0]).request(url.origin + req.url, {headers: req.headers, method:req.method}, (resp => {
             resp.pipe(res)
         res.write(`<script>setInterval(()=>{Array.from(document.querySelectorAll("[href]")).forEach(r=>{r.href.startsWith(location.origin)||"a"==r.nodeName||(r.href=location.origin+"/asset:"+r.href)}),Array.from(document.querySelectorAll("a")).forEach(r=>{r.href.startsWith(location.origin)||(r.href=location.origin+"/proxyto:"+r.href)}),Array.from(document.querySelectorAll("[src]")).forEach(r=>{r.src.startsWith(location.origin)||(r.src=location.origin+"/asset:"+r.src)})},750);</script>`)
         }))
