cfcec4578d1a349ff0a3ec1c323f3dfa907a2fd7
DeviousPear <146679233+DeviousPear@users.noreply.github.com>
2024-01-12 10:47:34 -0800

    Update cool shit.js

--- cool shit.js
+++ cool shit.js
@@ -1,12 +1,18 @@
-const ORIGIN_REQUEST = "request";
 setInterval(() => {
+    Array.from(document.querySelectorAll("a")).forEach((r) => {
+        if (!r.href.includes(location.origin)) {
+            r.href = location.origin + "/to:" + r.href
+        }
+    })
     Array.from(document.querySelectorAll("[href]")).forEach((r) => {
-        r.href.startsWith(location.origin) || "a" == r.nodeName || (r.href = location.origin + "/asset:" + r.href);
-    }),
-        Array.from(document.querySelectorAll("a")).forEach((r) => {
-            r.href.startsWith(location.origin) || (r.href = location.origin + "/proxyto:" + r.href);
-        }),
-        Array.from(document.querySelectorAll("[src]")).forEach((r) => {
-            r.src.startsWith(location.origin) || (r.src = location.origin + "/asset:" + r.src);
-        });
+        if (!r.href.includes(location.origin)) {
+            r.href = location.origin + "/asset:" + r.href
+        }
+    })
+    
+    Array.from(document.querySelectorAll("[src]")).forEach((r) => {
+        if (!r.src.includes(location.origin)) {
+            r.src = location.origin + "/asset:" + r.src
+        }
+    });
 }, 750);
