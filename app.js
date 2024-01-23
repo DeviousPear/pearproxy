fda3bf6c754684afd1c902332a1ecd4606edacbd
pear_man <rgpearman@icloud.com>
2024-01-22 20:25:29 -0800

    shoddy shoddington does it work

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var proxy = require("http").createServer((req, res) => {
         res.statusCode = 303
         res.setHeader("Location", stuff.pathname + stuff.search)
         res.end("Opening " + stuff.toString())
-    } else if (req.url.startsWith("/asset:")) {
+    } else if (req.url.startsWith("/INJECTSCRIPT")) res.write(`setInterval(()=>{Array.from(document.querySelectorAll("a")).forEach(r=>{console.log(r.getAttribute("href")),new URL(r.getAttribute("href")).origin!=location.origin&&(r.href=location.origin+"/to:"+r.href)}),Array.from(document.querySelectorAll("[href]")).forEach(r=>{console.log(r.getAttribute("href")),new URL(r.getAttribute("href")).origin!=location.origin&&(r.href=location.origin+"/asset:"+r.href)}),Array.from(document.querySelectorAll("[src]")).forEach(r=>{console.log(r.getAttribute("src")),new URL(r.getAttribute("src")).origin!=location.origin&&(r.src=location.origin+"/asset:"+r.src)})},750);`)
+    else if (req.url.startsWith("/asset:")) {
         let fakeHeaders = {}
         fakeHeaders["User-Agent"] = req.headers["user-agent"]
         console.log("proxy on")
@@ -15,6 +16,9 @@ var proxy = require("http").createServer((req, res) => {
         let url = new URL(require("cookie").parse(req.headers.cookie).pearproxy)
         require(url.protocol.split(":")[0]).get(url.origin + req.url, {headers: fakeHeaders}, resp => {
             resp.pipe(res)
+            if (resp.headers["content-type"].contains("text/html")) {
+                res.write(`<script src="/INJECTSCRIPT"></script>`)
+            }
         })
     } else if (req.url == "/CHECKHEADERS") {
         res.setHeader("Content-Type", "application/json")
@@ -27,7 +31,7 @@ var proxy = require("http").createServer((req, res) => {
         let url = new URL(require("cookie").parse(req.headers.cookie).pearproxy)
         require(url.protocol.split(":")[0]).get(url.origin + req.url, {headers: fakeHeaders}, (resp => {
             resp.pipe(res)
-        res.write(`<script src="/INJECTSCRIPT"></script>`)
+            res.write(`<script src="/INJECTSCRIPT"></script>`)
         }))
     } else {
         console.log(req.url)
